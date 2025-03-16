85fef19fc2f879d1d078a79940bb320130ad1cd5
Nred1 <nouryredouane@gmail.com>
2025-03-16 02:05:11 -0400

    mettre à jour le fichier doctorModel.js

--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -16,5 +16,6 @@ const doctorSchema = new mongoose.Schema({
     date: { type: Number, required: true },
 }, { minimize: false })
 
+
 const doctorModel = mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
 export default doctorModel;
